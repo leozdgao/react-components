d22cf3f3de2961c6fd2bf9e4e3bfa5f77f85fdae
leozdgao <gzdleo0816@gmail.com>
2015-07-27 16:29:32 +0800

    change return value of listenEvent

--- src/mixins/event.js
+++ src/mixins/event.js
@@ -2,8 +2,10 @@ import eventListener from 'lgutil/dom/eventListener';
 
 export default {
   listenEvent (target, eventType, callback) {
-    this._events ? this._events.push(eventListener(target, eventType, callback))
-      : this._events = [eventListener(target, eventType, callback)];
+    this._events ? this._events.push(eventListener.listen(target, eventType, callback))
+      : this._events = [eventListener.listen(target, eventType, callback)];
+
+    return this._events[this._events.length - 1];
   },
   clearAllEventListener () {
     this._events && (this._events.forEach((e) => e.remove()), this._events = []);
